5bf8f48d32d2bae4bca1caacaf896ae3b27c168c
Ahmad Arif <ahmad.arif019@gmail.com>
2018-03-03 15:58:24 +0700

    fix instruction after add unit testing

--- instructions.js
+++ instructions.js
@@ -4,10 +4,10 @@ const path = require('path')
 
 module.exports = async function (cli) {
   try {
-    await cli.makeConfig('swagger.js', path.join(__dirname, './config/swagger.js'))
+    await cli.makeConfig('swagger.js', path.join(__dirname, './templates/config.js'))
     cli.command.completed('create', 'config/swagger.js')
 
-    await cli.copy(path.join(__dirname, './public/docs'), 'public/docs')
+    await cli.copy(path.join(__dirname, './templates/docs'), 'public/docs')
     cli.command.completed('copy assets', 'public/docs')
   } catch (error) {
     // ignore errors
